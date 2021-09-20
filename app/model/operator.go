0fe6cb342eba051004daa3fe14c4fa3260abc530
Monkeyman <2918508198@qq.com>
2021-09-21 02:08:09 +0800

    :speech_balloon: chore:更换操作符编码

--- app/model/operator.go
+++ app/model/operator.go
@@ -2,8 +2,8 @@ package model
 
 // 操作符定义
 var (
-	baseOperator   = []string{"+", "-", "*", "/", "()"}
-	juniorOperator = []string{"(square)", "^2"}
+	baseOperator   = []string{"+", "-", "×", "÷", "()"}
+	juniorOperator = []string{"√", "²"}
 	highOperator   = []string{"tan", "sin", "cos"}
 	finalOperator  = "="
 )

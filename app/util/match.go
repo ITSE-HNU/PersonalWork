64ac68ce8baec1c186ee9e5b1af77cb97c26de80
Monkeyman <2918508198@qq.com>
2021-09-20 22:15:27 +0800

    :recycle: feat:增加判断逻辑

---
+++ app/util/match.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"regexp"
+)
+
+// CheckMode 使用正则进行模式检查
+func CheckMode(str string) (int, string, error) {
+	matched, err := regexp.MatchString("切换为小学", str)
+	if err != nil {
+		return 0, "", err
+	}
+	if matched {
+		return 1, "小学", nil
+	}
+	matched, err = regexp.MatchString("切换为初中", str)
+	if err != nil {
+		return 0, "", err
+	}
+	if matched {
+		return 2, "初中", nil
+	}
+	matched, err = regexp.MatchString("切换为高中", str)
+	if err != nil {
+		return 0, "", err
+	}
+	if matched {
+		return 3, "高中", nil
+	}
+	matched, err = regexp.MatchString("切换为", str)
+	if err != nil {
+		return 0, "", err
+	}
+	if matched {
+		return 4, "", nil
+	}
+	return -1, "", nil
+}

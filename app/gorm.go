d9a3818973daa2fa025a65493189c2579bc0dd0c
Monkeyman <2918508198@qq.com>
2021-09-25 14:21:47 +0800

    :zap: fix: 调整gorm日志输出等级，解决提示slow sql，增加logger config

--- app/gorm.go
+++ app/gorm.go
@@ -5,6 +5,9 @@ import (
 	"gitee.com/itse/personal-work/app/entity"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
 	"time"
 )
 
@@ -31,7 +34,19 @@ func NewDB() (*gorm.DB, error) {
 	c := config.C.DB
 	cGorm := config.C.GORM
 
-	db, err := gorm.Open(sqlite.Open(c.DSN()), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容）
+		logger.Config{
+			SlowThreshold:             2 * time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Silent,   // 日志级别
+			IgnoreRecordNotFoundError: true,            // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,           // 禁用彩色打印
+		},
+	)
+
+	db, err := gorm.Open(sqlite.Open(c.DSN()), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		return nil, err
 	}
